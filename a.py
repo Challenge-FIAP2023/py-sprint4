dd2bfd2f4fe2337d6e4aaa1734a505bcbf4ade42
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-23 11:24:04 -0300

    tratar erro

---
+++ a.py
@@ -0,0 +1,5 @@
+n = input(': ')
+if int(n):
+    print('numero inteiro')
+else:
+    print('nao')
--- teste-funcao-erro.py
+++ teste-funcao-erro.py
@@ -1,48 +1,53 @@
-def tratarErro(input='',tipo=0):
+def tratarErro(inputTratamento='',tipo=''):
     '''
     Função para tratar erros:
     - Inteiro
     - String
-    :param input: Frase do sistema para o usuário
+    :param inputTratamento: Frase do sistema para o usuário
     :param tipo: Tipo string (1) ou tipo inteiro (2)  
     '''
     try:
 
-        if tipo == 1:
+        inputTratamento = bool(inputTratamento)
+        if tipo == 'string':
             # Se o tipo a ser tratado for string
+            if not int(inputTratamento):
+                print('string')
 
-        elif tipo == 2:
+            
+
+        elif tipo == 'int':
             # Se o tipo a ser tratado for inteiro
+            inputTratamento = int(inputTratamento)
+            print('inteiro')
 
         else:
             # Se o tipo a ser tratado for diferente de 1 e 2
-            raise ValueError("\033[31mTipo inválido:! O tipo deve ser 1 (string) ou 2 (inteiro).\033[m")
+            raise ValueError("\033[31mTipo inválido:! O tipo deve ser <string> ou <int>.\033[m")
 
     except Exception as e:
         print(f"Erro: {e}")
-        resposta = None 
 
-
-    return resposta
 
 
 '''
 Inputs que precisam de tratamento de erro 
 
-Linha 71: continuar = int(input('\nSelecione uma opção: '))
+- Linha 71: continuar = int(input('\nSelecione uma opção: '))
 
-Linha 165: resp = int(input('\nSelecione uma opção: '))
+- Linha 165: resp = int(input('\nSelecione uma opção: '))
 
-Linha 215: duvida = int(input('\nQual é o número da sua dúvida? '))
+- Linha 215: duvida = int(input('\nQual é o número da sua dúvida? '))
 
-Linha 275: sug = input('\nDigite a sua sugestão: ')
+- Linha 275: sug = input('\nDigite a sua sugestão: ')
 
-Linha 312: escolha_lixeira = int(input('\nQual lixeira você deseja verificar? '))
+- Linha 312: escolha_lixeira = int(input('\nQual lixeira você deseja verificar? '))
 
-Linha 404: perg = int(input('\nOpção: '))
+- Linha 404: perg = int(input('\nOpção: '))
 
 '''
 
 
-
+n = input(': ')
+tratarErro(n, 'string')
 
